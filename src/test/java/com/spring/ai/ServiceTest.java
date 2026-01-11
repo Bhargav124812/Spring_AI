892e1393e249f5049d3e818185b51bfce5c06691
Bhargav <gudurusaibhargav2002@gmail.com>
2026-01-11 13:35:56 +0530

    Embedding And Vector Search

--- src/test/java/com/spring/ai/ServiceTest.java
+++ src/test/java/com/spring/ai/ServiceTest.java
@@ -19,4 +19,27 @@ class ServiceTest {
         var joke = aiService.getContent("Bicep");
         System.out.println(joke);
     }
+
+    @Test
+    public void textEmbed(){
+        var embed=aiService.getEmbedding("This the a Text to Embed");
+        System.out.println(embed.length);
+        for(float e: embed) {
+            System.out.println(e+" ");
+        }
+    }
+
+    @Test
+    public void testStoreData() {
+        aiService.ingestDataToVectorStore();
+    }
+
+    @Test
+    public void testSimilaritySearch() {
+        var res = aiService.similaritySearch("The Movie released in 2010 and its genre is Sci-Fi");
+        for(var doc: res) {
+            System.out.println(doc);
+        }
+
+    }
 }
